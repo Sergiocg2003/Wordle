d6a9bca88313ae95f2edb1e26a1c0c813ba17cdd
Sergiocg2003 <scorgon265@g.educaand.es>
2023-03-09 20:29:15 +0100

    Cambio de nombres

--- src/main/java/com/api/wordle/WordleApplication.java
+++ src/main/java/com/api/wordle/WordleApplication.java
@@ -1,4 +1,4 @@
-package com.example.wordle;
+package com.api.wordle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/api/wordle/WordleApplicationTests.java
+++ src/test/java/com/api/wordle/WordleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.wordle;
+package com.api.wordle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
